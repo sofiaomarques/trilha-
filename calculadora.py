48f40d6abfecfbda6dbb54f55ff9ad8846f35544
Sofia de Oliveira Marques <sofiaomarques26@gmaile.com>
2026-06-27 13:04:00 -0300

    Adiciona questao_6

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+def somar(a, b):
+    return a + b
+
+def multiplicar(a, b):
+
+    return a * b
+
+if __name__ == '__main__':
+
+    print(somar(10, 5))
+
+    print(multiplicar(4, 3))
---
+++ main.py
@@ -0,0 +1,4 @@
+import calculadora
+print(calculadora.somar(10, 5))
+
+print(calculadora.multiplicar(4, 3))
